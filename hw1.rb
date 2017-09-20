e12f5eb3f1a2652b5d644ff3c812b207743e8a46
stacyjchang <stacy.chang@berkeley.edu>
2017-09-20 15:05:24 -0700

    first attempt of hw1

--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,21 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-
+  a**2 + b**2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+  b = a.sort
+  b.each do |i|
+    i = i + 1
+  end
+  b
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+  result = nil
+  (0...(last_name.length)).each do |idx|
+    break result = first_name + last_name[(idx + 1)..-1] if first_name.end_with?(last_name[0..idx])
+  end
+  result
 end
 
 def blockin_time(a)
