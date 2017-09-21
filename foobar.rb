3ef6c04e677736b11a4a08acb47e061ec8fb5358
stacyjchang <stacy.chang@berkeley.edu>
2017-09-20 23:08:56 -0700

    finished foobar

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,7 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map!{|i| i.to_i}.map!{|i| i += 2}.select{|i| i if i % 2 == 0}.uniq.select{|i| i if i <= 10}.sum
   end
 end
 
